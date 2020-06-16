12ec20c4192054f9a8ad8927d5a6682b181f5d0c
Gabi Cocan <gabriel.calin.cocan@gmail.com>
2020-06-16 18:57:32 +0300

    add Tema9

---
+++ Tema9/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{cout<<"Introdu nr de mere pe care le are Ioana"<<endl;
+int x;
+cin>>x;
+cout<<"Cu cate mere are mai multe Maria?"<<endl;
+int z;
+cin>>z;
+int Ma=x+z;
+cout<<"Ioana are "<<x<<" mere"<<endl;
+cout<<"Maria are "<<Ma<<" mere"<<endl;
+int Mi=Ma*2;
+cout<<"Mihai are "<<Mi<<" mere"<<endl;
+int S=x+Ma+Mi;
+cout<<"In total au "<<S<<" mere";
+return 0;
+
+
+}
